8ac7b0cdc44c7cc83907cbf05b384bee0b13d543
Dalibor Filipovikj <filipovikj@kimisuite.com>
2025-11-18 22:27:57 +0100

    Initial commit

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,29 +1,6 @@
-variable "aws_region" {
-  description = "AWS region to deploy into"
-  type        = string
-}
-
-variable "aws_profile" {
-  description = "Shared credentials profile name"
-  type        = string
-  default     = "default"
-}
-
 variable "vpc_cidr" {
   description = "Define VPC CIDR Block"
   default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_count" {
-  description = "Number of public subnets"
-  type        = number
-  default     = 2
-}
-
-variable "private_subnet_count" {
-  description = "Number of private subnets"
-  type        = number
-  default     = 1
 }
 
 variable "public_subnet_cidrs" {
@@ -36,4 +13,10 @@ variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
   default     = ["10.0.3.0/24"]
+}
+
+variable "tags" {
+  description = "Common tags applied to every resource in this module"
+  type        = map(string)
+  default     = {}
 }
